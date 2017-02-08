4de98a16771cd0e4e1615767ed972d0591bbf7eb
Travis Vander Hoop <vanderhoop@me.com>
2017-02-08 15:46:25 -0500

    REFACTORS continue

--- web/static/js/app.js
+++ web/static/js/app.js
@@ -8,22 +8,28 @@
 
 let video_out = document.getElementById("vid-box")
 
-function login(form) {
-  var phone = window.phone = PHONE({
-      number        : form.username.value || "Anonymous", // listen on username line else Anonymous
-      publish_key   : 'pub-c-774ec798-47ab-4add-a4e0-4db1282eeb45',
-      subscribe_key : 'sub-c-dd561ef6-edf9-11e6-8bf6-02ee2ddab7fe',
-  });
-  phone.ready(function(){ form.username.style.background="#55ff5b"; });
-  phone.receive(function(session){
-      session.connected(function(session) { video_out.appendChild(session.video); });
-      session.ended(function(session) { video_out.innerHTML=''; });
-  });
-  return false;   // So the form does not submit.
+let login = form => {
+  let phone = window.phone = PHONE({
+    number: form.username.value,
+    publish_key: 'pub-c-774ec798-47ab-4add-a4e0-4db1282eeb45',
+    subscribe_key: 'sub-c-dd561ef6-edf9-11e6-8bf6-02ee2ddab7fe',
+    ssl: true,
+  })
+
+  phone.ready(() => { form.username.style.background="#55ff5b" })
+  phone.receive((session) => {
+    session.connected((session) => { video_out.appendChild(session.video) })
+    session.ended((session) => { video_out.innerHTML='' })
+  })
+
+  return false
 }
 
-function makeCall(form){
-  if (!window.phone) alert("Login First!");
-  else phone.dial(form.number.value);
-  return false;
+let makeCall = form => {
+  if (!window.phone) alert("Login First!")
+  else phone.dial(form.number.value)
+  return false
 }
+
+window.login = login
+window.makeCall = makeCall
