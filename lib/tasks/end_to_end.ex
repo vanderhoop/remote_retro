541e323ef79b0b40632129f40f8f74f16d116ef9
Travis Vander Hoop <vanderhoop@me.com>
2017-02-19 19:41:16 -0700

    Return exit code from end_to_end task
      - to avoid false positives on CircleCI, we need to return the exit code from invoking mix test --only feature_test

--- lib/tasks/end_to_end.ex
+++ lib/tasks/end_to_end.ex
@@ -3,10 +3,12 @@ defmodule Mix.Tasks.EndToEnd do
 
   @shortdoc "Runs feature tests"
   def run(_) do
-    System.cmd(
+    { _, exit_code } = System.cmd(
       "mix",
       ["test", "--only", "feature_test", "--color"],
       into: IO.stream(:stdio, :line)
     )
+
+    exit_code
   end
 end
