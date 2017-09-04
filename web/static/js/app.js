61688f66768c9a3c8f39052cccda8c4a38ad7103
Travis Vander Hoop <vanderhoop@me.com>
2017-09-04 11:18:06 -0600

    Add comment

--- web/static/js/app.js
+++ web/static/js/app.js
@@ -21,6 +21,7 @@ const configureStore = (rootReducer) => {
     window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
   )
 
+  // ensures that updates to reducers are hot reloaded
   if (module.hot) {
     module.hot.accept('./reducers/index', () => {
       const nextRootReducer = require('./reducers/index').default
