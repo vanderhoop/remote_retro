24f0b8f6fe52af0813689ecc3cca77a93efbe848
Travis Vander Hoop <vanderhoop@me.com>
2017-10-07 09:33:07 -0600

    check_origin: false

--- config/prod.exs
+++ config/prod.exs
@@ -14,7 +14,8 @@ use Mix.Config
 config :remote_retro, RemoteRetro.Endpoint,
   http: [port: {:system, "PORT"}],
   url: [scheme: "https", host: System.get_env("HEROKU_HOST"), port: 443],
-  force_ssl: [host: nil],
+  force_ssl: [rewrite_on: [:x_forwarded_proto]],
+  check_origin: false,
   cache_static_manifest: "priv/static/manifest.json",
   secret_key_base: System.get_env("SECRET_KEY_BASE")
 
