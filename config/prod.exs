6297d98946323abb89bba67fd2d6101f3fc620c3
Travis Vander Hoop <vanderhoop@me.com>
2017-10-07 08:52:07 -0600

    ensure force_ssl production config uses custom domain

--- config/prod.exs
+++ config/prod.exs
@@ -14,7 +14,7 @@ use Mix.Config
 config :remote_retro, RemoteRetro.Endpoint,
   http: [port: {:system, "PORT"}],
   url: [scheme: "https", host: System.get_env("HEROKU_HOST"), port: 443],
-  force_ssl: [rewrite_on: [:x_forwarded_proto], host: nil],
+  force_ssl: [rewrite_on: [:x_forwarded_proto], host: System.get_env("HEROKU_HOST")],
   cache_static_manifest: "priv/static/manifest.json",
   secret_key_base: System.get_env("SECRET_KEY_BASE")
 
