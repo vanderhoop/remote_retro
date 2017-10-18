be01dceb8c3143d3d206f3661c7cffc3f2ee45d9
Travis Vander Hoop <vanderhoop@me.com>
2017-10-17 18:44:27 -0600

    Lint in webpack dev server

--- webpack.devserver.js
+++ webpack.devserver.js
@@ -1,7 +1,8 @@
 const webpack = require("webpack")
+// eslint-disable-next-line import/no-extraneous-dependencies
 const WebpackDevServer = require("webpack-dev-server")
-
 const config = require("./webpack.config")
+
 const { port } = config.devServer
 
 new WebpackDevServer(webpack(config), {
@@ -10,10 +11,10 @@ new WebpackDevServer(webpack(config), {
   compress: true,
   headers: { "Access-Control-Allow-Origin": "*" },
   stats: { modules: false },
-}).listen(port, "0.0.0.0", (err, result) => {
+}).listen(port, "0.0.0.0", err => {
   if (err) console.error(err)
   console.log(`[info] Running webpack-dev-server using http://localhost:${port}`)
-  console.log(`[info] Webpack compiling assets...\n`)
+  console.log("[info] Webpack compiling assets...\n")
 })
 
 // Exit on end of STDIN
