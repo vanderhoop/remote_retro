18505e8fdf8992b451febf9db347c739cfdb09be
Travis Vander Hoop <vanderhoop@me.com>
2017-10-20 08:44:53 -0600

    fixup into remote retro component reversion commit

--- web/static/js/dev/enable_hot_css_module_updates.js
+++ web/static/js/dev/enable_hot_css_module_updates.js
@@ -1,10 +1,15 @@
 export default () => {
   const hotEmitter = require("webpack/hot/emitter") // eslint-disable-line global-require
-  const DEAD_CSS_TIMEOUT = 2000
+  const DEAD_CSS_TIMEOUT = 5000
 
   hotEmitter.on("webpackHotUpdate", () => {
-    document.querySelectorAll("link[href][rel=stylesheet]").forEach(link => {
+    const stylesheets = document.querySelectorAll("link[href][rel=stylesheet]")
+    const nodeListAsArray = Array.apply(null, stylesheets);
+    const appStylesheets = nodeListAsArray.filter(stylesheet => /app\.css/.test(stylesheet.href))
+
+    appStylesheets.forEach(link => {
       const nextStyleHref = link.href.replace(/(\?\d+)?$/, `?${Date.now()}`)
+
       const newLink = link.cloneNode()
       newLink.href = nextStyleHref
 
