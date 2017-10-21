d90ba1b1d49be91f72612fe71e94630c365ecefb
Travis Vander Hoop <vanderhoop@me.com>
2017-10-21 11:34:05 -0600

    Intercept erroneous Provider warning from react-redux

--- web/static/js/configure_store.js
+++ web/static/js/configure_store.js
@@ -11,6 +11,11 @@ export default () => {
 
   // ensures that updates to reducers are hot reloaded
   if (module.hot) {
+    const oldError = console.error
+    console.error = (text) => {
+      if (/<Provider> does not support/.test(text)) { return }
+      oldError(text)
+    }
     module.hot.accept("./reducers/index", () => {
       const nextRootReducer = require("./reducers/index").default
       store.replaceReducer(nextRootReducer)
