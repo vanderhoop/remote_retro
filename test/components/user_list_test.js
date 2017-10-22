09a3f388235b1b6892405c8e0f1835ee569a143d
Travis Vander Hoop <vanderhoop@me.com>
2017-10-22 13:58:19 -0600

    Wip

--- test/components/user_list_test.js
+++ test/components/user_list_test.js
@@ -4,7 +4,7 @@ import { shallow, mount } from "enzyme"
 import UserList from "../../web/static/js/components/user_list"
 import UserListItem from "../../web/static/js/components/user_list_item"
 
-describe("passed an array of users", () => {
+describe.only("passed an array of users", () => {
   const users = [{
     given_name: "treezy",
     online_at: 803,
@@ -23,5 +23,19 @@ describe("passed an array of users", () => {
   it("sorts the users by their arrival in the room, ascending", () => {
     const wrapper = mount(<UserList users={users} />)
     expect(wrapper.text()).to.match(/zandertreezy/i)
+  })
+
+  describe("when the stage is 'closed'", () => {
+    it("displays an iframe", () => {
+      const wrapper = mount(<UserList users={users} stage="closed" />)
+      expect(wrapper.find("iframe").length).to.equal(1)
+    })
+  })
+
+  describe("when the stage is not 'closed'", () => {
+    it("does not display an iframe", () => {
+      const wrapper = mount(<UserList users={users} stage="idea-generation" />)
+      expect(wrapper.find("iframe").length).to.equal(0)
+    })
   })
 })
--- web/static/js/components/user_list.jsx
+++ web/static/js/components/user_list.jsx
@@ -3,8 +3,8 @@ import UserListItem from "./user_list_item"
 import * as AppPropTypes from "../prop_types"
 import styles from "./css_modules/user_list.css"
 
-const UserList = props => {
-  const usersSortedByArrival = props.users.sort((a, b) => a.online_at - b.online_at)
+const UserList = ({ users, stage }) => {
+  const usersSortedByArrival = users.sort((a, b) => a.online_at - b.online_at)
 
   const listItems = usersSortedByArrival.map(user =>
     <UserListItem key={user.online_at} user={user} />
@@ -12,6 +12,17 @@ const UserList = props => {
 
   return (
     <section className={`${styles.index} ui center aligned basic segment`}>
+      { stage === "closed" &&
+        <iframe
+          src="http://ghbtns.com/github-btn.html?user=stride-nyc&repo=remote_retro&type=watch&count=true"
+          allowTransparency="true"
+          scrolling="0"
+          width="100"
+          height="20"
+          frameBorder="0"
+        />
+      }
+
       <ul id="user-list" className="ui horizontal list">
         {listItems}
       </ul>
